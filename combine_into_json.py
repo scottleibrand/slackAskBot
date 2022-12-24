0bf565201620a6bc67a575aac38b4788331675b6
Scott Leibrand <scottleibrand@gmail.com>
2022-12-23 21:34:13 -0800

    Get and write out all embeddings.

---
+++ combine_into_json.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import json
+import csv
+import sys
+
+# Check for all three arguments
+if len(sys.argv) != 4:
+    print('Usage: combine_into_json.py <data_dir> <embeddings_dir> <output_file>')
+    sys.exit(1)
+
+# Get arguments
+data_dir = sys.argv[1]
+embeddings_dir = sys.argv[2]
+output_file = sys.argv[3]
+
+all_data = []
+
+# Open output file
+with open(output_file, 'w') as outfile:
+    # Iterate over subdirectories in data directory
+    for subdir in sorted(os.listdir(data_dir)):
+        subdir_path = os.path.join(data_dir, subdir)
+
+        # Iterate over json files in subdirectory
+        for filename in sorted(os.listdir(subdir_path)):
+            if filename.endswith('.json'):
+                file_path = os.path.join(subdir_path, filename)
+
+                # Read data json
+                with open(file_path) as data_file:
+                    data_json = json.load(data_file)
+
+                # Read embeddings json
+                embeddings_path = os.path.join(embeddings_dir, subdir, filename)
+                if os.path.exists(embeddings_path) and os.path.getsize(embeddings_path) > 0:
+                    print(f'Found embeddings file for {subdir_path}/{filename} at {embeddings_path}')
+                    with open(embeddings_path) as embeddings_file:
+                        embeddings_json = json.load(embeddings_file)
+                        # Add embeddings json to all_data    
+                else:
+                    #print('Warning: embeddings file not found for {}'.format(filename))
+                    print(f'Warning: embeddings file not found for {subdir_path}/{filename} at {embeddings_path}')
+                    continue
+
+                # Add data json and embeddings json to all_data
+                all_data.append({'messages': data_json, 'ada_search': embeddings_json})
+
+    # Write all record to output file, pretty-printed
+    #json.dump({'messages': data_json, 'ada_search': embeddings_json}, outfile, indent=4)
+    json.dump(all_data, outfile, indent=4)
---
+++ get-all-embeddings.py
@@ -0,0 +1,90 @@
+import openai
+from openai.embeddings_utils import get_embedding
+import sys
+import os
+import tiktoken
+
+def get_embeddings_old(input_file, output_file):
+    # Open the input file for reading and the output file for writing
+    with open(input_file, 'r') as input_f, open(output_file, 'w') as output_f:
+        # Iterate over the lines in the input file
+        for line in input_f:
+            # Get the embedding for the line
+            embedding = get_embedding(line, engine='text-embedding-ada-002')
+            
+            # Write the embedding to the output file as a string
+            output_f.write(str(embedding) + '\n')
+
+def get_embeddings(input_file, output_file):
+    # Open the input file for reading and the output file for writing
+    with open(input_file, 'r') as input_f:
+        # Iterate over the lines in the input file
+        for line in input_f:
+            # Get the token count of the line
+            enc = tiktoken.get_encoding("gpt2")
+            tokens = enc.encode(line)
+            line_token_count = len(tokens)
+            
+            # If the token count is >8000, split the line into as many equal-sized chunks as needed so each chunk is <8000 tokens
+            if line_token_count > 8000:
+                num_chunks = line_token_count // 8000
+                print(f'{input_file} is too long ({line_token_count} tokens), splitting into {num_chunks+1} chunks')
+                for i in range(num_chunks+1):
+                    chunk = line[i*8000:(i+1)*8000]
+                    # Get the embedding for the chunk
+                    embedding = get_embedding(chunk, engine='text-embedding-ada-002')
+                    # Write out the chunk embedding to its own file
+                    print(f'Writing chunk {i+1} of {num_chunks+1} to {output_file}-chunk-{i+1}')
+                    with open(output_file + "-chunk-" + str(i+1), 'w') as chunk_f:
+                        chunk_f.write(str(embedding) + '\n')
+            else:
+                # Get the embedding for the line
+                embedding = get_embedding(line, engine='text-embedding-ada-002')
+                # Write the embedding to the output file as a string
+                with open(output_file, 'w') as output_f:
+                    output_f.write(str(embedding) + '\n')
+
+# Check that the correct number of command-line arguments were provided
+if len(sys.argv) < 3:
+    print('Usage: python get_embeddings.py input_dir output_dir [subdirs]')
+    sys.exit(1)
+
+# Get the input and output directory paths from the command-line arguments
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]
+
+# Create the output directory if it does not exist
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
+# Get the subdirectory names from the command-line arguments, or use all subdirectories if none provided
+subdirs = sys.argv[3:]
+if not subdirs:
+    subdirs = os.listdir(input_dir)
+
+# Iterate over all the subdirectories in the input directory
+for root, dirs, files in sorted(os.walk(input_dir)):
+    # Only process subdirectories with the provided names, or all subdirectories if none provided
+    dirname = os.path.basename(root)
+    if subdirs and dirname not in subdirs:
+        continue
+    # Create the same subdirectories in the output directory
+    subdir = root.replace(input_dir, output_dir)
+    if not os.path.exists(subdir):
+        os.mkdir(subdir)
+    # Iterate over all the files in the subdirectory
+    for file in sorted(files):
+        input_file = os.path.join(root, file)
+        output_file = os.path.join(subdir, file)
+        # Skip the file if the output file already exists and is more than 0 bytes
+        if os.path.exists(output_file) and os.path.getsize(output_file) > 0:
+            print(f'Skipping {subdir}/{file} because output file already exists')
+            continue
+        # Skip the file if a chunk of the output file already exists
+        if os.path.exists(output_file + "-chunk-1"):
+            print(f'Skipping {subdir}/{file} because output file chunks already exist')
+            continue
+        # Print the file name
+        print(f'Processing {subdir}/{file}')
+        # Process the file and write the output to an identically named file in the output subdirectory
+        get_embeddings(input_file, output_file)
