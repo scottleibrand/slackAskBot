1ea298eedbc810f45aaedf383836ca61bafd709c
Scott Leibrand <scottleibrand@gmail.com>
2022-12-23 21:28:43 -0800

    Combine and split files of messages into parts based on token count.

--- combine_messages.py
+++ combine_messages.py
@@ -1,3 +1,12 @@
+
+"""
+This script splits large files of data into smaller parts based on the number of tokens in each part.
+It takes in data from the 'trimmed' directory and splits it into parts with a maximum of 1000 tokens each.
+The parts are then written to the 'combined' directory.
+The script also combines multiple files into one if the total number of tokens is less than 1000.
+The output files are named with the start and end dates of the data they contain.
+"""
+
 import os
 import json
 import tiktoken
@@ -37,141 +46,6 @@ def splitdata(data, tokens_threshold):
         current_length += item_length
     parts.append(current_part)
     return parts
-
-def oldmain():
-    tokens_threshold=1000
-    maxtokens = 3000
-
-    # Create the combined directory if it doesn't exist
-    combined_dir = 'combined/'
-    if not os.path.exists(combined_dir):
-        os.makedirs(combined_dir)
-
-    # Iterate through the trimmed directory
-    for subdir in os.listdir('trimmed/'):
-        # Create the same subdirectory in combined
-        combined_subdir = os.path.join(combined_dir, subdir)
-        
-        # Initialize the token count and start/end dates and the combined data
-        token_count = 0
-        start_date = None
-        end_date = None
-        combined_data = []
-        split_data = []
-        
-        # Iterate through the files in the subdirectory
-        # If the current file tokens are greater than maxtokens, write out any previous files,
-        #   then split the current file into multiple parts and write out each part
-        # Else if the combined token count plus the current file tokens are greater than tokens_threshold,
-        #   then write out the combined messages and reset the combined data w/ the current file's messages
-        # Else the cumulative token count plus the current file tokens is less than tokens_threshold, so continue to read in the next file
-        
-
-        for filename in sorted(os.listdir(os.path.join('trimmed/', subdir))):
-            # Read in the file
-            with open(os.path.join('trimmed/', subdir, filename)) as f:
-                data = json.load(f)
-            
-            # Get the token count of the file's messages
-            file_token_count = get_token_count(data)
-            print(f'{subdir}/{filename}: {file_token_count} tokens')
-            if start_date is None:
-                start_date = filename.split('.')[0]
-            if end_date is None:
-                end_date = filename.split('.')[0]
-            
-            # If the cumulative token count is less than tokens_threshold, combine the messages and continue to read in the next file
-            if token_count + file_token_count < tokens_threshold:
-                token_count += file_token_count
-                # Update the start and end dates
-                end_date = filename.split('.')[0]
-                # Combine the messages
-                for message in data:
-                    combined_data.append(message)
-                print(f'{filename} combined token count: {token_count+file_token_count} tokens: continuing')
-                continue
-            elif token_count + file_token_count < maxtokens:
-                if token_count < maxtokens:
-                    # Write out the previous messages
-                    if not os.path.exists(combined_subdir):
-                        os.makedirs(combined_subdir)
-                        if start_date is not None and end_date is not None:
-                            output_filename = start_date + '-' + end_date + '.json'
-                            print(f'{filename} combined token count: {token_count+file_token_count} tokens: writing out previous {token_count} tokens')
-                            # Write out the combined messages
-                            if combined_data is not None:
-                                with open(os.path.join(combined_subdir, output_filename), 'w') as f:
-                                    json.dump(combined_data, f)
-                else:
-                    # Split the previous messages
-                    split_data = splitdata(combined_data, tokens_threshold)
-                    if split_data is not None:
-                        for i, message in enumerate(split_data):
-                            if not os.path.exists(combined_subdir):
-                                os.makedirs(combined_subdir)
-                            output_filename = start_date + '-' + end_date + '-part' + str(i) + '.json'
-                            print(f'Writing out {output_filename} with {get_token_count(message)} tokens')
-                            with open(os.path.join(combined_subdir, output_filename), 'w') as f:
-                                json.dump(message, f)
-                # If the cumulative token count is between tokens_threshold and maxtokens, write out the combined messages first
-                if not os.path.exists(combined_subdir):
-                    os.makedirs(combined_subdir)
-        
-                print(f'{filename} combined token count: {token_count+file_token_count} tokens: writing out previous {token_count} tokens')
-                # Create the output filename
-                if start_date is not None and end_date is not None:
-                    output_filename = start_date + '-' + end_date + '.json'
-                    # If there is no combined data, write out the current file
-                    if len(combined_data) == 0:
-                        for message in data:
-                            combined_data.append(message)
-                    # Write out the combined messages
-                    if combined_data is not None:
-                        with open(os.path.join(combined_subdir, output_filename), 'w') as f:
-                            json.dump(combined_data, f)
-            else: # token_count + file_token_count >= maxtokens
-                # If the cumulative token count is greater than maxtokens call a split function
-                print(f'{filename} combined token count: {token_count+file_token_count} tokens: splitting')
-                split_data = splitdata(data, tokens_threshold)
-                #print(split_data)
-                # Write out the split messages into multiple files
-                for i, message in enumerate(split_data):
-                    if not os.path.exists(combined_subdir):
-                        os.makedirs(combined_subdir)
-                    output_filename = start_date + '-' + end_date + '-part' + str(i) + '.json'
-                    print(f'Writing out {output_filename} with {get_token_count(message)} tokens')
-                    with open(os.path.join(combined_subdir, output_filename), 'w') as f:
-                        json.dump(message, f)
-
-
-
-            # Reset the start/end dates
-            start_date = None
-            end_date = None
-            # Reset the combined data
-            combined_data = data
-            # Reset the token count to the current file's token count
-            token_count = file_token_count
-
-            
-            
-
-        # If any data remains, write it out
-        if len(combined_data) > 0:
-            # Create the output filename
-            if start_date is None:
-                start_date = filename.split('.')[0]
-            if end_date is None:
-                end_date = filename.split('.')[0]
-            
-            if not os.path.exists(combined_subdir):
-                os.makedirs(combined_subdir)
-
-            output_filename = start_date + '-' + end_date + '.json'
-            # Write out the combined messages
-            with open(os.path.join(combined_subdir, output_filename), 'w') as f:
-                json.dump(combined_data, f)
-        #exit()
 
 def main():
     tokens_threshold=1000
